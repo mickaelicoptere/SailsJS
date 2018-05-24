e834069ffd143a74d838fc59cfd457315d7ae150
Mickael Lanier <mickael.lanier@etu.unice.fr>
2018-05-24 12:16:28 +0200

    :rocket: tp4 fini :ok_hand:

--- S4/TP4/script.js
+++ S4/TP4/script.js
@@ -10,9 +10,31 @@ function init() {
     } else {
         console.log("ERREUR @ XMLHTTPREQUEST")
     }
+    myRequest.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200){
+            console.log("Requête AJAX effectuée");
+            loadData();
+        }
+    };
 }
 
 function loadData () {
+    document.getElementById("selection").addEventListener("change", function(event) {
+        console.log(event.target.value);
+        send(event);
+        document.getElementById("data").innerHTML = this.responseText;
+    })
+}
+
+function send(event) {
+    myRequest.open('GET',proxy + event.target.value, true);
+    console.log(event.target.value);
+    myRequest.send();
+}
+
+
+/* Deprecated */
+function buttonLoadData () {
     myRequest.onreadystatechange = function () {
         if (this.readyState == 4&& this.status == 200){
             console.log("Requête AJAX effectuée");
